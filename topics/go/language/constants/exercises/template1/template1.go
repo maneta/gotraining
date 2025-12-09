a39e1152f6a9eb6a0b4182275b0408a2bba27c6c
Daniel Cesario <bender@Daniels-MacBook-Pro.local>
2025-12-09 11:41:32 +0100

    constant exercise

--- topics/go/language/constants/exercises/template1/template1.go
+++ topics/go/language/constants/exercises/template1/template1.go
@@ -7,19 +7,24 @@
 package main
 
 // Add imports.
+import "fmt"
 
 const (
-// Declare a constant named server of kind string and assign a value.
-
-// Declare a constant named port of type integer and assign a value.
+	// Declare a constant named server of kind string and assign a value.
+	server string = "localhost"
+	// Declare a constant named port of type integer and assign a value.
+	port int16 = 8080
 )
 
 func main() {
 
 	// Display the value of both server and port.
+	fmt.Printf("Server: %s\n", server)
+	fmt.Printf("Port: %d\n", port)
 
 	// Divide a constant of kind integer and kind floating point and
 	// assign the result to a variable.
-
+	minutes := 5320 / 60.0
+	fmt.Printf("Minutes: %f\n", minutes)
 	// Display the value of the variable.
 }
